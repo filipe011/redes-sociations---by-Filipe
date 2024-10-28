5ec1d9a18b9d0f4e7c560fc919f5b7b3e5c4aa4b
filipe011 <127518103+filipe011@users.noreply.github.com>
2024-10-28 09:54:56 -0300

    Add files via upload

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,10 +1,40 @@
+import { getCSS } from "./common.js"
+
 async function quantidadeUsuarios() {
-    cosnt url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
-    cosnt dados = await res.json()
+    const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
     
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar', 
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Rede sociais com mais usuários no mundo',
+            x: 0,
+            front: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--front')
+            }
+        }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuarios()
