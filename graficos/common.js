9fac3f4b9e4b24acdd6c86543baecb98042c0d38
filipe011 <127518103+filipe011@users.noreply.github.com>
2024-11-18 09:50:11 -0300

    Add files via upload

--- graficos/common.js
+++ graficos/common.js
@@ -8,5 +8,23 @@ const tickConfig = {
         family: getCSS('--font')
 }
 
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config);
+}
 
-export {getCSS, tickConfig}
+function incluirTexto(text) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = text
+    container.appendChild(paragrafo)
+}
+
+export {getCSS, tickConfig, criarGrafico, incluirTexto}
